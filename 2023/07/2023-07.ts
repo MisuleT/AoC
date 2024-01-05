9cf17af79cfa6370a5370dce4b7ed2b22c1457d7
Michaela Tesařová <mitesarova@csas.cz>
2024-01-05 19:20:52 +0100

    day 7 part two

--- 2023/07/2023-07.ts
+++ 2023/07/2023-07.ts
@@ -6,6 +6,7 @@ type Hand = {
 }
 
 const CARD_ORDER = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+const CARD_ORDER_WITH_JOKER = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'];
 
 function getStrength(hand: string) {
     const cards = hand.split('').sort();
@@ -32,6 +33,41 @@ function getStrength(hand: string) {
         return 5;
     return 6;
 }
+function getStrengthWithJokers(hand: string) {
+    const cards = hand.split('').filter(card => card !=='J');
+    const cardsSize = new Set(cards).size;
+    let jokersCount = (hand.match(/J/g) || []).length;
+
+    if(!jokersCount){
+        return getStrength(hand);
+    }
+    // five of kind
+    if(jokersCount === 5) return 0;
+    if(jokersCount === 4) return 0;
+    if(cardsSize === 1) return 0;
+    // four of kind
+    if(jokersCount > 1 && cardsSize === 2) return 1;
+
+    if(jokersCount === 2) {
+        // Three of a kind
+        if(cardsSize === 3) return 3;
+    }
+    if(jokersCount === 1) {
+        if(cardsSize === 2) {
+            // full house
+            if(cards[0] === cards[1] && cards[2] === cards[3]) return 2;
+            if(cards[0] === cards[3] && cards[1] === cards[2]) return 2;
+            if(cards[0] === cards[2] && cards[1] === cards[3]) return 2;
+            // four of kind
+            return 1;
+        }
+        // Three of a kind
+        if(cardsSize === 3) return 3;
+        // One pair
+        if(cardsSize === 4) return 5;
+    }
+    return 6;
+}
 
 function compareCards(a: Hand, b: Hand) {
     if(b.strength !== a.strength) {
@@ -40,6 +76,19 @@ function compareCards(a: Hand, b: Hand) {
     for (let i = 0; i < a.cards.length; i++) {
         if(a.cards[i] !== b.cards[i]){
             return CARD_ORDER.indexOf(b.cards[i]) - CARD_ORDER.indexOf(a.cards[i])
+        }
+    }
+    return 0;
+}
+
+function compareCardsWithJokers(a: Hand, b: Hand) {
+    if(b.strength !== a.strength) {
+        return b.strength - a.strength;
+    }
+    for (let i = 0; i < a.cards.length; i++) {
+        if(a.cards[i] !== b.cards[i]){
+            return CARD_ORDER_WITH_JOKER.indexOf(b.cards[i]) - CARD_ORDER_WITH_JOKER.indexOf(a.cards[i])
+
         }
     }
     return 0;
@@ -57,11 +106,21 @@ export const part1 = (input: string) => {
             }
         })
         .sort(compareCards)
-    console.log(hands)
-
 
     return hands.reduce((total, hand, index) => total + hand.bet * (index+1), 0);
 }
 export const part2 = (input: string) => {
-    return 0
+    const hands: Hand[] = input
+        .split('\n')
+        .map(line => line.split(" "))
+        .map(([cards, bet]) => {
+            return{
+                cards,
+                bet: Number(bet),
+                strength: getStrengthWithJokers(cards)
+            }
+        })
+        .sort(compareCardsWithJokers)
+
+    return hands.reduce((total, hand, index) => total + hand.bet * (index+1), 0);
 }
--- 2023/07/__tests__/2023-07.test.ts
+++ 2023/07/__tests__/2023-07.test.ts
@@ -23,16 +23,20 @@ QQQJA 483`
 
     describe('part 2 mock', () => {
         it('sample', () => {
-            const input = ``
-            expect(part2(input)).toBe(249483956);
+            const input = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+            expect(part2(input)).toBe(5905);
         });
 
         it('real', () => {
-            const input = readFileSync('2023/06/input.txt', {
+            const input = readFileSync('2023/07/input.txt', {
                 encoding: 'utf8',
                 flag: 'r'
             });
-            expect(part2(input)).toBe(0);
+            expect(part2(input)).toBe(252137472);
         });
     });
 });
