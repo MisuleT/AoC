a31a31544c9de26b638aa14664aef766b128dfc5
Michaela Tesarova <mitesarova@csas.cz>
2024-02-08 23:53:50 +0100

    2023 day 14 part one

---
+++ 2023/14/2023-14.ts
@@ -0,0 +1,45 @@
+export const part1 = (input: string) => {
+    const grid = input.split('\n').map(line => line.split(''));
+    for (let y = 0; y < grid.length; y++) {
+        //look for any Os
+        for (let x = 0; x < grid[y].length; x++) {
+
+            if (grid[y][x] === "O") {
+                //if you find one, start scanning upwards
+                for (let i = y - 1; i >= 0; i--) {
+                    //if you find a # or a O, stop scanning
+                    if (grid[i][x] === "#" || grid[i][x] === "O") {
+                        //move the O to here
+                        grid[y][x] = ".";
+                        grid[i + 1][x] = "O";
+                        break;
+                    }
+
+                    if (i === 0) {
+                        grid[y][x] = ".";
+                        grid[0][x] = "O";
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    console.log(grid);
+    let sum = 0;
+    let multiplyBy = grid.length +1;
+    for (let i = 0; i < grid.length; i++) {
+        let count = 0;
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 'O') {
+                count++;
+            }
+        }
+        multiplyBy--;
+        sum += (count * multiplyBy);
+    }
+    return sum;
+}
+
+export const part2 = (input: string) => {
+    return 0;
+}
---
+++ 2023/14/__tests__/2023-14.test.ts
@@ -0,0 +1,43 @@
+import {readFileSync} from "fs";
+import {part1, part2} from "../2023-14";
+describe('advent of code 14', () => {
+    const input = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+    describe('part 1', () => {
+
+        it('sample', () => {
+
+            expect(part1(input)).toBe(136);
+        });
+
+        it('real', () => {
+            const input = readFileSync('2023/14/input.txt', {
+                encoding: 'utf8',
+                flag: 'r'
+            });
+            expect(part1(input)).toBe(108759);
+        });
+    });
+
+    describe('part 2', () => {
+        it('sample', () => {
+            expect(part2(input)).toBe(0);
+        });
+
+        it('real', () => {
+            const input = readFileSync('2023/14/input.txt', {
+                encoding: 'utf8',
+                flag: 'r'
+            });
+            expect(part2(input)).toBe(0);
+        });
+    });
+});
