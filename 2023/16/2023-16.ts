77fac4e20ac835347d96e15bd8ad14099e83dd76
Michaela Tesarova <mitesarova@csas.cz>
2024-03-01 11:28:22 +0100

    2023 day 16 animation

--- 2023/16/2023-16.ts
+++ 2023/16/2023-16.ts
@@ -52,32 +52,62 @@ function printMap(map: string[][], visited: Set<string>) {
     console.log(buffer.join(''))
 }
 
+let ENERGIZED: [number, number, number][] = [];
+
 function bfs(startPos: [number, number, string], map: string[][]) {
     let que: [number, number, string][] = [startPos];
     let visited = new Set<string>();
     let energized = new Set<string>();
     visited.add(`${startPos[0]}, ${startPos[1]}, ${startPos[2]}`);
     energized.add(`${startPos[0]}, ${startPos[1]}`);
+    let lasers = 1;
     while (que.length > 0) {
         let [x, y, direction] = que.shift()!;
         let nextPositions = getNextPositions(x, y, direction, map[y][x]);
+        if(nextPositions.length > 1){
+            lasers++;
+        }
         for (const [nx, ny, nDirection] of nextPositions) {
-            if (nx < 0 || ny < 0 || nx >= map.length || ny >= map.length) continue;
-            if (visited.has(`${nx}, ${ny}, ${nDirection}`)) continue;
+            if (nx < 0 || ny < 0 || nx >= map.length || ny >= map.length) {
+                lasers--;
+                continue;
+            }
+            if (visited.has(`${nx}, ${ny}, ${nDirection}`)) {
+                lasers--;
+                continue;
+            }
             que.push([nx, ny, nDirection]);
             visited.add(`${nx}, ${ny}, ${nDirection}`);
             energized.add(`${nx}, ${ny}`);
+            ENERGIZED.push([nx, ny, lasers]);
         }
     }
     // printMap(map, energized)
     return energized.size;
 }
 
+function render(map: string[][]) {
+    const interval = setInterval(()=> {
+        const  [x,y, lasers] = ENERGIZED.shift()!;
+        map[y][x] = '*';
+        for (let i = 0; i < lasers; i++) {
+            const  [x,y] = ENERGIZED.shift()!;
+            map[y][x] = '*';
+        }
+        console.clear();
+        console.log(map.map(line => line.join('')).join('\n'));
+        if(!ENERGIZED.length) clearInterval(interval);
+    }, 1000/25);
+}
+
 export const part1 = (input: string) => {
     const map = input.split('\n').map(a => a.split(''));
 
     const startPos: [number, number, string] = [0, 0, 'R']
-    return bfs(startPos, map);
+    const result = bfs(startPos, map);
+    // render(map);
+
+    return result;
 }
 export const part2 = (input: string) => {
     const map = input.split('\n').map(a => a.split(''));
